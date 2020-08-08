0f96e20bdd38da8e689ec3adfbe45c7b8e4795f6
esmy1990 <61815853+esmy1990@users.noreply.github.com>
2020-08-09 00:31:34 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,9 @@
+import csv
+with open('example_stats.csv', newline='') as csvfile:
+ data = csv.DictReader(csvfile)
+ print("Name Average Response Time")
+ print("---------------------------------")
+ for row in data:
+   print(row['Name'], row['Average Response Time'])
+Copy
+departments.csv
