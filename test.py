0ef676fe2aab37a51485453e710a4ed03f7ca456
esmy1990 <61815853+esmy1990@users.noreply.github.com>
2020-08-09 09:39:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,14 @@
 import csv
-with open('example_stats.csv', newline='') as csvfile:
- data = csv.DictReader(csvfile)
- print("Name Average Response Time")
- print("---------------------------------")
- for row in data:
-   print(row['Name'], row['Average Response Time'])
+with open('example_stats.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+
+    read_l = [{key:value for key, value in row.items() if key in ('Name', 'Average Response Time')}
+              for row in reader]
+
+
+with open('new.csv', 'w') as csvfile:
+    fieldnames = read_l[0].keys()
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+    writer.writeheader()
+    for row in read_l[0:]:
+        writer.writerow(row)
