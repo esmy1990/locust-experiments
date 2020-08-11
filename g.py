0a1278ae7708101cdad9eb243c40768fd92d5268
esmy1990 <61815853+esmy1990@users.noreply.github.com>
2020-08-11 20:00:18 +0530

    Add files via upload

---
+++ g.py
@@ -0,0 +1,14 @@
+from csv import reader
+
+with open('new.csv', 'r') as read_obj:
+    csv_reader = reader(read_obj)
+    header = next(csv_reader)
+    flag = 0
+    for row in csv_reader:
+        for field in row[1:]:
+            #try:
+                field = float(field)
+                if (field >= 100):
+                    flag = 1
+                    break
+print(flag)
